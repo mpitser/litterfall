a86f4b17fbac0ea43ff3eac942e84b3ed2cf02dc
Andrew Ruether <aruethe2@swarthmore.edu>
2012-12-13 19:22:01 -0500

    Account for two types of data tables

--- DB_translation_scripts/litterfall_mysql2mongo.py
+++ DB_translation_scripts/litterfall_mysql2mongo.py
@@ -25,22 +25,40 @@ cur.execute("SELECT distinct location from jlm_litterfall_observation")
 for r in cur.fetchall():
    print r[0]
 
-exit(0)
+
 
 # Get all the observations
-cur.execute("SELECT o.sample_id, o.location, o.plot, o.colltype, o.observerA, o.observerB, o.observerC, o.notes, o.collection_date, o.sky_condition, o.precipitation_condition from jlm_litterfall_observation as o")
+observations_missing_data = []
+
+cur.execute("SELECT o.sample_id, o.row_id, o.location, o.plot, o.colltype, o.observerA, o.observerB, o.observerC, o.notes, o.collection_date, o.sky_condition, o.precipitation_condition from jlm_litterfall_observation as o where o.row_id")
 
 for r in cur.fetchall():
 
 	print r
 
 	# Get each datapoint for this observation
+	# There are two tables -- if we can't find observations in one, try the second
 	d = conn.cursor()
-	d.execute("SELECT d.trap, d.sample_type, d.value from jlm_litterfall_observation as o ,  jlm_litterfall_data3 as d where d.sample_id = '%s'" % r[0])
+	d.execute("SELECT d.trap, d.sample_type, d.value from jlm_litterfall_data as d where d.sample_id = %d" % r[1])
+	
+	if d.rowcount==0:
+		# Try alternate table
+		d.execute("SELECT d.trap, d.sample_type, d.value from jlm_litterfall_data3 as d where d.sample_id = '%s'" % r[0])
+		
+	if d.rowcount!=0:
+		print "\tNumber of obserbations: %d" % d.rowcount
+		#for datapoint in d.fetchall():
+			#print datapoint
+	else:
+		print "**** NO DATAPOINTS ***"
+		# Keep track of observations that don't have datapoints
+		observations_missing_data.append(r[0])
 
-	for datapoint in d.fetchall():
-		print datapoint
-
-
+print "Sample ID's missing observations:"
+print observations_missing_data
 
 #cur.execute("SELECT o.location, o.plot, o.colltype, o.observerA, o.observerB, o.observerC, o.notes, o.collection_date from jlm_litterfall_observation as o and  jlm_litterfall_data3 as d where jlm_litterfall_observation.sample_id = jlm_litterfall_data3.sample_id")
+
+d.close()
+cur.close()
+conn.close()
