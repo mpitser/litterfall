f23a7a714a8e2beddfaeb60ba9577ba31a264b86
Andrew Ruether <aruethe2@swarthmore.edu>
2012-12-13 20:27:15 -0500

    Mongo connection working

--- DB_translation_scripts/litterfall_mysql2mongo.py
+++ DB_translation_scripts/litterfall_mysql2mongo.py
@@ -2,6 +2,8 @@
 
 import pymysql
 import ConfigParser
+from pymongo import MongoClient
+
 
 # Need to start with SSH tunnel to Moodle host
 # e.g. ssh -n -N -f -L 3306:localhost:3306 aruethe2@acadweb.swarthmore.edu
@@ -14,18 +16,27 @@ MySQL_user = Config.get('MySQL', 'user')
 MySQL_pw = Config.get('MySQL', 'pw')
 MySQL_db = Config.get('MySQL', 'db')
 
-print MySQL_db
 
-conn = pymysql.connect(host=MySQL_host, port=3306, user=MySQL_user, passwd=MySQL_pw, db=MySQL_db)
-cur = conn.cursor()
 
-# Get all the sites
+# Connect to MySQL
+mysql_db = pymysql.connect(host=MySQL_host, port=3306, user=MySQL_user, passwd=MySQL_pw, db=MySQL_db)
+
+# Cnnect to MongoDB
+mongo = MongoClient('ec2-107-22-11-44.compute-1.amazonaws.com', 27017)
+mongo_db = mongo.machado_db
+
+
+
+# Get all the sites from MySQL and put them into MongoDB
+cur = mysql_db.cursor()
 cur.execute("SELECT distinct location from jlm_litterfall_observation")
+sites = mongo_db.sites
 
 for r in cur.fetchall():
-   print r[0]
+   	print r[0]
+	site_id = sites.save({"_id": r[0]})
 
-
+exit()
 
 # Get all the observations
 observations_missing_data = []
@@ -38,7 +49,7 @@ for r in cur.fetchall():
 
 	# Get each datapoint for this observation
 	# There are two tables -- if we can't find observations in one, try the second
-	d = conn.cursor()
+	d = mysql_db.cursor()
 	d.execute("SELECT d.trap, d.sample_type, d.value from jlm_litterfall_data as d where d.sample_id = %d" % r[1])
 	
 	if d.rowcount==0:
@@ -46,9 +57,9 @@ for r in cur.fetchall():
 		d.execute("SELECT d.trap, d.sample_type, d.value from jlm_litterfall_data3 as d where d.sample_id = '%s'" % r[0])
 		
 	if d.rowcount!=0:
-		print "\tNumber of obserbations: %d" % d.rowcount
-		#for datapoint in d.fetchall():
-			#print datapoint
+		print "\tNumber of observations: %d" % d.rowcount
+		for datapoint in d.fetchall():
+			print datapoint
 	else:
 		print "**** NO DATAPOINTS ***"
 		# Keep track of observations that don't have datapoints
@@ -57,8 +68,9 @@ for r in cur.fetchall():
 print "Sample ID's missing observations:"
 print observations_missing_data
 
-#cur.execute("SELECT o.location, o.plot, o.colltype, o.observerA, o.observerB, o.observerC, o.notes, o.collection_date from jlm_litterfall_observation as o and  jlm_litterfall_data3 as d where jlm_litterfall_observation.sample_id = jlm_litterfall_data3.sample_id")
 
 d.close()
 cur.close()
-conn.close()
+mysql_db.close()
+
+
