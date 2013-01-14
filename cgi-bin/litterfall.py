8d751040e3de941a3ef8a65a8d4c728877fd7f58
X.D.Zhai <xzhai1@swarthmore.edu>
2013-01-14 22:03:42 +0000

    attempted put options

--- cgi-bin/litterfall.py
+++ cgi-bin/litterfall.py
@@ -1,25 +1,14 @@
 #! /usr/bin/python
 from datetime import datetime
+from pymongo import MongoClient
+from bson import json_util
 import json
 import cgi
 import cgitb; cgitb.enable()
 import os
-from pymongo import MongoClient
-from bson import json_util
 import ConfigParser
-
-# some global variables
-query = cgi.FieldStorage()
-plot = query.getvalue('plot')
-site = query.getvalue('site')
-quadrant = query.getvalue('quadrant')
-tree_id = query.getvalue('tree_id')
-species = query.getvalue('species')
-angle = query.getvalue('angle')
-distance = query.getvalue('distance')
-diameter = query.getvalue('diameter')
-	
-def getdata(obs):
+		
+def getdata(obs, site, plot):
 	if site == 'all':
 		data = obs.find({'collection_type':'tree'}).distinct('site')
 		for j in range(0,len(data)):
@@ -40,10 +29,9 @@ def getdata(obs):
 	ser_data = json.dumps(json_data, default=json_util.default, separators=(',', ':'))
 	print ser_data
 
-def postdata(obs):	
-	stuff = {'posting data': 'lalala'}
-	ser_stuff = json.dumps(stuff, default=json_util.default, separators=(',', ':'))
-	print ser_stuff
+def postdata(obs, data):
+	print json.dumps(data, default=json_util.default, separators=(',', ':'))
+	#obs.save(data)
 	
 def main():
 	# Load config (for database info, etc)
@@ -59,38 +47,26 @@ def main():
 	# Use MongoDB observation collection
 	obs = mongo_db.observations
 	
-	method = os.environ['REQUEST_METHOD']
-
-	print 'Content-Type: application/json\n'
+	#method = os.environ['REQUEST_METHOD']
+	
+	debug = os.environ
+	debug['REQUEST_METHOD'] = 'PUT'
+	method = debug['REQUEST_METHOD']
+	
+	#print 'Content-Type: application/json\n'
+	print 'Content-Type: text/html\n'
+	print method
 	
 	if method == 'GET':
-		getdata(obs)
-	elif method == 'POST':
-		postdata(obs)
-			
+		query = cgi.FieldStorage()
+		plot = query.getvalue('plot')
+		site = query.getvalue('site')
+		getdata(obs, site, plot)
+	elif method == 'PUT':
+		data = {'_id':'13246876532135'}
+		print debug
+		postdata(obs, data)
+
+		
 if __name__ == "__main__":
     main()
-    
-# hierarchy: site > plot > quadrant
-# let's see how many sites there are
-'''
-# for each site, plots and quadrants, find out how many trees there are
-for site in sites:
-    print 'site:', site
-    num_plots = len(obs.find({'collection_type':'tree', 'site': site}).distinct('plot'))
-    print 'number of plots:', num_plots
-    print
-    
-    # for each plot
-    for plot in range(1,num_plots+1):
-        num_quads = len(obs.find({'collection_type':'tree',
-                                  'site': site, 'plot': plot}).distinct('quadrant'))
-
-        # for each quadrant
-        for quadrant in range(1,num_quads+1):
-            num_trees = obs.find({'collection_type':'tree',
-                                      'site': site, 'plot': plot, 'quadrant': quadrant}).count()
-            print 'site', site, 'plot', plot, 'quadrant', quadrant
-            print 'has', num_trees
-            print
-'''
