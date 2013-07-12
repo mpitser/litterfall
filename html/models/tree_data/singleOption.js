219cec2da915030310d4d1556bd13345998e3e42
Boyd <boyd.chayanon@gmail.com>
2013-07-12 18:24:50 +0000

    comment for singleOption.js

--- html/models/tree_data/singleOption.js
+++ html/models/tree_data/singleOption.js
@@ -1,3 +1,11 @@
+/*
+Model: singleOption
+Represents: an option that represents each site
+--------------------
+
+Well, there is nothing much, as you can probably see. It's essentially an empty model.
+
+*/
 define([
   'underscore',
   'backbone'
