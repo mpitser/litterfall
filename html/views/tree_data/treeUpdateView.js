e02d4c2f806fe279a1e5b4cf6cb23c854343f7a2
jadams1 <jadams1@swarthmore.edu>
2013-07-12 12:39:20 -0400

    Update treeUpdateView.js

--- html/views/tree_data/treeUpdateView.js
+++ html/views/tree_data/treeUpdateView.js
@@ -2,7 +2,7 @@ define([
 	'jquery',
 	'underscore', 
 	'backbone',
-	'tree_data/models/Tree'
+	'models/tree_data/Tree'
 ], function($, _, Backbone, Tree){
 	var treeUpdateView = Backbone.View.extend({
 		tagName: 'div',
