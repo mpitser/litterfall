85a701760d563f505f8ab7f382e3d25427a278f6
jadams1 <jadams1@swarthmore.edu>
2013-07-12 12:41:05 -0400

    Update plotRowUpdateView.js

--- html/views/tree_data/plotRowUpdateView.js
+++ html/views/tree_data/plotRowUpdateView.js
@@ -2,7 +2,7 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'tree_data/models/Tree'
+  'models/tree_data/Tree'
 ], function($, _, Backbone, Tree){
 	var plotRowUpdateView = Backbone.View.extend({
 		tagName: 'tr',				
