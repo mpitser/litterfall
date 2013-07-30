48971d02cd17f789313b2f72d8976e1608a19147
mpitser <m.pitser@gmail.com>
2013-07-30 17:41:25 +0000

    fixed typeaheads for the litterfall observer entry fields

--- html/app.js
+++ html/app.js
@@ -5,19 +5,20 @@ define([
   'router' // Request router.js
 ], function($, _, Backbone, Router){
   var initialize = function(){
-  
+  	console.log("initializing the typeahead");
 	var orig = {
 		matcher: $.fn.typeahead.Constructor.prototype.matcher,
 		updater: $.fn.typeahead.Constructor.prototype.updater,
 		select: $.fn.typeahead.Constructor.prototype.select,
 		listen: $.fn.typeahead.Constructor.prototype.listen,
-		render: $.fn.typeahead.Constructor.prototype.render
+		render: $.fn.typeahead.Constructor.prototype.render,
 	};
   
 	$.extend($.fn.typeahead.Constructor.prototype, {
 		matcher: function(item) {
-			if (this.options.type == 'observers') {
-
+			if (this.options.type == 'observersList') {
+				/* typeahead that begins new after each comma, supports multiple entries in input field */
+				
 				var observers = this.query.split(",");
 				var last_observer = observers[observers.length - 1];
 				var last_observer = last_observer.replace(/^\s+/,"");
@@ -26,11 +27,25 @@ define([
 				
 				for (i = 0; i < observers.length - 2; i++) {
 					if (observers[i].replace(/^\s+|\s+$/g, '') == item) return false;
+					// take off all active classes
+					$(document).find('li .active').removeClass("active");	
 				}
 				
 				var last_observer = last_observer.toLowerCase();
 				
 				return last_observer.length && ~item.toLowerCase().indexOf(last_observer);
+				
+			} else if (this.options.type == 'observer') {
+				/* typeahead to enter one observer at a time */
+				
+				// TODO: not sure how much of this is important, go back through
+				this.$element.parent().removeClass("error");
+				var observer = this.query.toLowerCase();
+				console.log(observer);
+				// take off all active classes
+				$(document).find('li .active').removeClass("active");
+				return observer.length && ~item.toLowerCase().indexOf(observer);
+			
 			} else if (this.options.type == 'species') {
 				
 				var is_matched = item.toLowerCase().indexOf(this.query.toLowerCase()) == 0;
@@ -45,54 +60,36 @@ define([
 				if (query_genus.toLowerCase() == item_genus.toLowerCase() && item.toLowerCase() == query_genus.toLowerCase() + " spp.") return true;
 				
 				return false;
-			} else if (this.options.type == 'observer') {
-				//console.log(this.query);
-				//console.log(this.options.source);
-				//console.log(this.options.source.indexOf(this.query.toString()) != -1);
-				//console.log(this.query == "");
-				
-				var is_matched = false;
-				for (i in this.options.source) {
-					if (this.options.source[i].toLowerCase().indexOf(this.query.toLowerCase()) == 0) is_matched = true;
-				}
-				
-				//console.log(is_matched);
-				if ((! is_matched) && (item != 'All Observers') && (this.query != "")) {
-					this.$element.parent().addClass("error");
-					this.$element.tooltip({title:"No such observer"}).tooltip("show");
-					return false;
-				} else {
-					this.$element.parent().removeClass("error");
-					this.$element.tooltip("destroy");
-					return true;
-				}
 			
 			} 
+			
 			return orig.matcher.call(this, item);
 			
 		},
 		updater: function(item) {
 			console.log("updater called");
-			if (this.options.type != 'observers') {
-				return orig.updater.call(this, item);
+
+			if (this.options.type == 'observersList') {
+				if (this.query.indexOf(",") == -1) {
+					console.log(item+", ");
+					return item+", ";
+				}
+				console.log(this.query.replace(/,[^,]*$/, ", "+item+", "));
+				return this.query.replace(/,[^,]*$/, ", "+item+", ");
 			}
 			
-			if (this.query.indexOf(",") == -1) return item+", ";
-			
-			return this.query.replace(/,[^,]*$/, ", "+item+", ");
-			
+			return orig.updater.call(this, item);			
+
 		},
 		select: function() {
 			console.log("select called");
-			if (this.options.type == 'observer') {
-				this.$element.parent().removeClass("error");
-				this.$element.parent().tooltip("destroy");
-				return orig.select.call(this);
-			} else if (this.options.type == 'species') {
+			
+			if (this.options.type == 'species') {
 				var to_return = orig.select.call(this);
 				this.$element.focus();
 				return to_return;
 			} 
+			
 			return orig.select.call(this);
 
 		},
@@ -107,7 +104,8 @@ define([
 				});
 			}
 			orig.listen.call(this);
-		}/*,
+		}
+		/*,
 		render: function(items) {
  			console.log(this.options.item);
      		for (i in items) {
