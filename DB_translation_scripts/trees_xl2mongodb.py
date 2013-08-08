ae988fe705933872d31542c603c7705aed169ee7
mpitser <m.pitser@gmail.com>
2013-08-08 20:03:09 +0000

    modularized the code for getting the diameter entries by year. built in the status of the tree based on the notes associated with the tree

--- DB_translation_scripts/trees_xl2mongodb.py
+++ DB_translation_scripts/trees_xl2mongodb.py
@@ -26,15 +26,38 @@ mongo_db = mongo[MongoDB_db]
 # Use MongoDB observation collection
 observations = mongo_db.tree_observations
 
-def checkStatus():
-	status = "alive"
-	print "hello in checkStatus"
+def getStatus(notes, observation):
+
+	#status_tree = observation['status']
 	# change Status if needed
+	if "missing" in notes:
+		observation['status'] = "missing"
+	elif ("fallen" in notes or "fell" in notes) and ("dead" in notes or "dead" in observation['status']):
+		observation['status'] = "dead_fallen"
+	elif "standing" in notes or "dead" in notes:
+		observation['status'] = "dead_standing"
+	else:
+		observation['status'] = "alive"
+	
 	# check future statuses based on Status
 	# if something new found in the notes, update the status
 	# else default that obs status to Status from last time
 	
-	return status
+	return observation['status']
+
+def addDiameterObservation(sheet, headers, rownum, observation, year):
+	try:
+		diam = sheet.row_values(rownum)[headers.index(str(year) + " DBH cm")]
+		notes = str(sheet.row_values(rownum)[headers.index(str(year) + " Comments")])
+		
+		# getting the status for this observation involves combing through the notes and also the previously set global tree status
+		status = getStatus(notes, observation)
+		
+		observation['diameter'].append({'value': diam, 'notes':notes, 'date': {'y': year, 'm': 01, 'd': 01}, 'status': status, 'observers': [] })
+	except ValueError:
+		print "No data from " + str(year)
+		
+	return
 
 for file in args.excel_filenames:
 	workbook = xlrd.open_workbook(file)
@@ -54,6 +77,7 @@ for file in args.excel_filenames:
 		# Start a new document
 		observation = {}
 		
+		# get the tree's meta-information
 		observation['collection_type'] = 'tree'
 		observation['site'] = sheet.row_values(rownum)[headers.index("Site Name")]
 		observation['plot'] = int(sheet.row_values(rownum)[headers.index("Plot number")])
@@ -80,96 +104,11 @@ for file in args.excel_filenames:
 			
 		observation['diameter'] = []
 		
-		# current state: Status is set to alive or dead_standing
 		
-		
-		# write for loop to go through years (upward), and make this script just add in the year
-		# call checkStatus(yearnotes, Status)
-		try:
-			dia_2009 = sheet.row_values(rownum)[headers.index("2009 DBH cm")]
-			notes_2009 = sheet.row_values(rownum)[headers.index("2009 Comments")]
-			if observation['status']:
-				status_2009 = 'dead_standing'
-			elif "dead" in notes_2009:
-				if "fallen" in notes_2009:
-					status_2009 = 'dead_fallen'
-				
-			else:
-				status_2009 = 'alive'
-			observation['diameter'].append({'value': dia_2009, 'notes':notes_2009, 'date': {'y': 2009, 'm': 01, 'd': 01}, 'status': status_2009, 'observers': [] })
-		except ValueError:
-			print "No 2009 data"
-	
-		try:
-			dia_2008 = sheet.row_values(rownum)[headers.index("2008 DBH cm")]
-			notes_2008 = sheet.row_values(rownum)[headers.index("2008 Comments")]
-			if observation['status']:
-				status_2008 = 'dead'
-			else:
-				status_2008 = 'alive'
-			observation['diameter'].append({'value': dia_2008, 'notes':notes_2008, 'date': {'y': 2008, 'm': 01, 'd': 01}, 'status': status_2008, 'observers': [] })
-		except ValueError:
-			print "No 2008 data"
+		# get all of the individual diameter information from the excel spreadsheet for a specific tree observation
+		for y in range(2002, 2009):
+			addDiameterObservation(sheet, headers, rownum, observation, y)
 			
-			
-		try:
-			dia_2007 = sheet.row_values(rownum)[headers.index("2007 DBH cm")]
-			notes_2007 = sheet.row_values(rownum)[headers.index("2007 Comments")]
-			if observation['status']:
-				status_2007 = 'dead'
-			else:
-				status_2007 = 'alive'
-			observation['diameter'].append({'value': dia_2007, 'notes':notes_2007, 'date': {'y': 2007, 'm': 01, 'd': 01}, 'status': status_2007, 'observers': [] })
-		except ValueError:
-			print "No 2007 data"
-			
-		try:
-			dia_2006 = sheet.row_values(rownum)[headers.index("2006 DBH cm")]
-			notes_2006 = sheet.row_values(rownum)[headers.index("2006 Comments")]
-			if observation['status']:
-				status_2006 = 'dead'
-			else:
-				status_2006 = 'alive'
-			observation['diameter'].append({'value': dia_2006, 'notes':notes_2006, 'date': {'y': 2006, 'm': 01, 'd': 01}, 'status': status_2006, 'observers': [] })
-		except ValueError:
-			print "No 2006 data"				
-			
-			
-		try:
-			dia_2005 = sheet.row_values(rownum)[headers.index("2005 DBH cm")]
-			notes_2005 = sheet.row_values(rownum)[headers.index("2005 Comments")]
-			if observation['status']:
-				status_2005 = 'dead'
-			else:
-				status_2005 = 'alive'
-			observation['diameter'].append({'value': dia_2005, 'notes':notes_2005, 'date': {'y': 2005, 'm': 01, 'd': 01}, 'status': status_2005, 'observers': [] })
-		except ValueError:
-			print "No 2005 data"		
-			
-		try:
-			dia_2003 = sheet.row_values(rownum)[headers.index("2003 DBH cm")]
-			notes_2003 = sheet.row_values(rownum)[headers.index("2003 Comments")]
-			if observation['status']:
-				status_2003 = 'dead'
-			else:
-				status_2003 = 'alive'
-			observation['diameter'].append({'value': dia_2003, 'notes':notes_2003, 'date': {'y': 2003, 'm': 01, 'd': 01}, 'status': status_2003, 'observers': [] })
-		except ValueError:
-			print "No 2003 data"
-			
-	
-		try:
-			dia_2002 = sheet.row_values(rownum)[headers.index("2002 DBH cm")]
-			notes_2002 = sheet.row_values(rownum)[headers.index("2002 Comments")]
-			if observation['status']:
-				status_2002 = 'dead'
-			else:
-				status_2002 = 'alive'
-			observation['diameter'].append({'value': dia_2002, 'notes':notes_2002, 'date': {'y': 2002, 'm': 01, 'd': 01}, 'status': status_2002, 'observers': [] })
-		except ValueError:
-			print "No 2002 data"	
-			
-					
 		print observation
 		
 		observation_id = observations.save(observation)
